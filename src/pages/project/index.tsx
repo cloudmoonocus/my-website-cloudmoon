1335d62b8dd2bd09c59b8959c5c2aaeaaf0e2990
cloudmoonocus <f356660222@foxmail.com>
2023-09-22 17:14:35 +0800

    fix: 调整代码

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,8 +1,50 @@
 import React, { useState } from "react";
-import { iconList, productIntro } from "./constant";
 import classNames from "classnames";
 import Layout from "@theme/Layout";
 import style from "./styles.module.css";
+
+const iconList = [
+  { src: "img/music-icon.png", alt: "网易云音乐" },
+  { src: "img/odin-icon.png", alt: "Odin低代码平台" },
+  { src: "img/g11n-icon.png", alt: "G11全球化作业平台" },
+  { src: "img/buxiwan-icon.png", alt: "不洗碗工作室官网" },
+];
+
+const productIntro = [
+  {
+    icon: "img/music-icon.png",
+    title: "网易云音乐（移动端）",
+    text: "仿网易云音乐移动端，功能包括但不限于登录账号，关注/取关，查看歌单，添加收藏，搜索等功能。",
+    img: "img/music.png",
+    link: "http://music.cloudmoon.asia",
+    bgi: "img/bgi1.jpg",
+  },
+  {
+    icon: "img/odin-icon.png",
+    title: "Odin低代码平台",
+    text: "Odin低代码平台是一款专注于网页创建的低代码构建平台，目的在于为有网站创建需求的个人或团队提供简单易操作的网页创建与设计平台，通过拖拉拽的操作方式和可视化的编辑界面，结合自定的数据配置，简单轻松地创建所需要的专属网页。",
+    img: "img/odin.png",
+    link: "http://odin.neuqer.com",
+    bgi: "img/bgi2.jpg",
+  },
+  {
+    icon: "img/g11n-icon.png",
+    title: "G11全球化作业平台",
+    text: "OpenEuler 全球化作业平台，翻译来自Gitee的Markdown文档，可以发布任务，申领翻译/审核任务，机器翻译，并可以将翻译结果上传至Gitee或Github。",
+    img: "img/g11n.png",
+    link: "http://g11n.cloudmoon.asia",
+    bgi: "img/bgi1.jpg",
+    disabled: true,
+  },
+  {
+    icon: "img/buxiwan-icon.png",
+    title: "不洗碗工作室官网",
+    text: "工作室官网，展示工作室相关信息，以及工作室的宣传。",
+    img: "img/buxiwan.png",
+    link: "http://buxiwan.cloudmoon.asia",
+    bgi: "img/bgi3.jpg",
+  },
+];
 
 const MyProject: React.FC = () => {
   const [index, setIndex] = useState<number>(0);
